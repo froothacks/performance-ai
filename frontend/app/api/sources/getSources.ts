7d7653b4e4e335d4cad954db7df8dd2bbf9b8392
Steven Xu <steven.xu1589@gmail.com>
2023-08-26 18:01:03 -0400

    Update api hooks

--- frontend/app/api/sources/getSources.ts
+++ frontend/app/api/sources/getSources.ts
@@ -1,22 +1,10 @@
 import { notFound } from 'next/navigation';
-import { BACKEND_URL } from '../utils';
-import { Sources } from './source';
+import { useSourceProvider } from '#/app/contexts/source-context';
 
-// `server-only` guarantees any modules that import code in file
-// will never run on the client. Even though this particular api
-// doesn't currently use sensitive environment variables, it's
-// good practise to add `server-only` preemptively.
-import 'server-only';
+export async function getSourcesForUser(userId: number) {
+  const [sourceMap] = useSourceProvider();
 
-export async function getSources(userId: number) {
-  const [isDarkTheme, setDarkTheme] = useLocalStorage(`source-${userId}`, true);
-
-  if (!res.ok) {
-    // Render the closest `error.js` Error Boundary
-    throw new Error('Something went wrong!');
-  }
-
-  const sources = (await res.json()) as Sources;
+  const sources = sourceMap?.[userId] ?? [];
 
   if (sources.length === 0) {
     // Render the closest `not-found.js` Error Boundary
