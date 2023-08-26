5cb008f6c451d5024705a6913e93bfddbbec0f85
Andrew-Chen-Wang <acwangpython@gmail.com>
2023-08-26 14:43:32 -0400

    Add more fastapi endpoints

--- src/config.py
+++ src/config.py
@@ -1,13 +1,16 @@
 import os
 
 import cohere
-import pymongo
+# import pymongo
+import motor.motor_asyncio
 from modal import Stub
 from slack_sdk.web.async_client import AsyncWebClient
 
 
-mongo_client = pymongo.MongoClient(os.getenv("MONGO_URI"))
-db = mongo_client.myDatabase
-# co = cohere.Client(os.getenv("COHERE_API_KEY"))
+# mongo_client = pymongo.MongoClient(os.getenv("MONGO_URI"))
+# db = mongo_client.myDatabase
+
+db = motor.motor_asyncio.AsyncIOMotorClient(os.getenv("MONGO_URI"))
+co = cohere.Client(os.getenv("COHERE_API_KEY"))
 modal_stub = Stub("example-fastapi-app")
-slack_client = AsyncWebClient()
+slack_client = AsyncWebClient(os.environ["SLACK_BOT_TOKEN"])
--- src/fastapi.py
+++ src/fastapi.py
@@ -1,45 +1,114 @@
-from typing import Optional
+from typing import TypedDict
 
-from fastapi import APIRouter, Header, Request
-from fastapi.templating import Jinja2Templates
+from fastapi import APIRouter, Request
 from modal import web_endpoint
 from pydantic import BaseModel
 
-from .config import modal_stub
+from .config import modal_stub, slack_client, db, co
 
 
 router = APIRouter()
-templates = Jinja2Templates(directory="templates")
+
+# class Item(BaseModel):
+#     name: str
+# @modal_stub.function()
+# @web_endpoint(method="POST")
+# def f(item: Item):
+#     return "Hello " + item.name
 
 
-# --- FastAPI app ---
-"""
-TODO Endpoints
-- All users
-- Current user
-- Given a prompt, give back top 5 examples
-"""
-
-
-class Item(BaseModel):
+class User(BaseModel):
+    id: str
     name: str
 
 
-@router.get("/")
-async def handle_root(request: Request, user_agent: Optional[str] = Header(None)):
-    print(f"GET /     - received user_agent={user_agent}")
-    return templates.TemplateResponse("index.html", {"request": request})
+async def get_slack_users():
+    class GetAllUsersSlackRespMemberProfile(TypedDict):
+        title: str
+        phone: str
+        skype: str
+        real_name: str
+        real_name_normalized: str
+        display_name: str
+        first_name: str
+        last_name: str
+        image_24: str
+        image_32: str
+        image_48: str
+        image_72: str
+        image_192: str
+        image_512: str
+        image_1024: str
+        status_text: str
+        status_emoji: str
+        status_expiration: int
+        avatar_hash: str
+        always_active: bool
+        team: str
+
+    class GetAllUsersSlackRespMember(TypedDict):
+        id: str
+        team_id: str
+        name: str
+        deleted: bool
+        color: str  # 757575
+        real_name: str
+        tz: str
+        tz_offset: int
+        profile: GetAllUsersSlackRespMemberProfile
+        is_bot: bool
+
+    class GetAllUsersSlackResp(TypedDict):
+        ok: bool
+        members: list[GetAllUsersSlackRespMember]
+
+    data: GetAllUsersSlackResp = (await slack_client.users_list()).data
+    return data
 
 
-@router.post("/foo")
-async def handle_foo(item: Item, user_agent: Optional[str] = Header(None)):
-    print(
-        f"POST /foo - received user_agent={user_agent}, item.name={item.name}"
-    )
-    return item
+@router.get("/all-users")
+async def get_all_users() -> list[User]:
+    """Gets all slack users
+    https://api.slack.com/methods/users.list
+    """
+    return [
+        User(id=x["id"], name=x["name"])
+        for x in (await get_slack_users())["members"]
+    ]
 
 
-@modal_stub.function()
-@web_endpoint(method="POST")
-def f(item: Item):
-    return "Hello " + item.name
+thread = db.test_thread_1
+
+
+@router.post("/slack-webhook")
+async def slack_webhook():
+    """
+    Processes Slack messages and stores in MongoDB
+
+    Collection schema:
+    {
+    "id": "mongo auto id",
+    "slack_thread_id": "slack thread id",
+    "thread averaged vector": "vector",
+    "thread text": "thread text",
+    "messages": {"id": "mongo id", "text": "message text", "vector"}
+    }
+    """
+
+    return ""
+
+
+class ThreadQuery(BaseModel):
+    prompt: str
+
+
+@router.post("/query-threads")
+async def query_threads(request: Request, data: ThreadQuery):
+    """Queries threads and returns top 5 results"""
+
+    prompt = data.prompt
+
+    document = await thread.find_one({'i': {'$lt': 1}})
+    cursor = await thread.find({'i': {'$lt': 5}}).sort('i')
+    for document in await cursor.to_list(length=100):
+        print(document)
