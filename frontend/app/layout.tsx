f885938fbc89473d3cd031835dc7cc5870f1164f
Steven Xu <steven.xu1589@gmail.com>
2023-08-26 16:45:45 -0400

    Remove unused files

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,6 @@
 import '#/styles/globals.css';
-import { AddressBar } from '#/ui/address-bar';
-import Byline from '#/ui/byline';
+import { SearchBar } from '#/ui/search-bar';
+import TeamCard from '#/ui/team-card';
 import { GlobalNav } from '#/ui/global-nav';
 import { Metadata } from 'next';
 
@@ -36,14 +36,14 @@ export default function RootLayout({
           <div className="mx-auto max-w-4xl space-y-8 px-2 pt-20 lg:px-8 lg:py-8">
             <div className="bg-vc-border-gradient rounded-lg p-px shadow-lg shadow-black/20">
               <div className="rounded-lg bg-black">
-                <AddressBar />
+                <SearchBar />
               </div>
             </div>
 
             <div className="bg-vc-border-gradient rounded-lg p-px shadow-lg shadow-black/20">
               <div className="rounded-lg bg-black p-3.5 lg:p-6">{children}</div>
             </div>
-            <Byline className="fixed sm:hidden" />
+            <TeamCard className="fixed sm:hidden" />
           </div>
         </div>
       </body>
